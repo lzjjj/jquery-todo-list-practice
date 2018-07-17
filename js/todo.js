7301da77cd78b97fc08c7a77c709a98001bde6f3
louis <louis.z.j.li@oocl.com>
2018-07-17 21:50:56 +0800

    [louis] complete the refactering

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,11 @@
 $(document)
     .ready(function () {
-
+        let todoList = [];
+        let filterList = [
+            { "dataFilter": "all", isSelect: true, content: "All" },
+            { "dataFilter": "active", isSelect: false, content: "Active" },
+            { "dataFilter": "complete", isSelect: false, content: "Complete" }
+        ];
         function generateUUID() {
             /*jshint bitwise:false */
             var i,
@@ -20,6 +25,74 @@ $(document)
             }
             return uuid;
         }
+        renderTodoList = () => {
+            return todoList.map(i => {
+                return `<li id="${i.id}" class="${i.isChecked ? 'checked' : ''}">
+                <input name="done-todo" type="checkbox" class="done-todo"> ${i.content} </li>`
+            })
+        }
+        renderFiltersList = () => {
+            return filterList.map(i => {
+                return `<li>
+                <a href="#" data-filter="${i.dataFilter}" class="${i.isSelect ? 'selected' : ''}" >${i.content}</a>
+            </li>`
+            })
+        }
+        renderToForm = () => {
+            return `<div>
+            <input class="input-text" type="text" name="ListItem" data-com.agilebits.onepassword.user-edited="yes">
+            <div id="button" onclick="addItem()">Add</div>
+        </div>
+        <br>
+        <ol>
+        ${renderTodoList().join("")}
+        </ol>
+        <div>
+            <ul id="filters">
+               ${renderFiltersList().join("")}
+            </ul>
 
-        // code to be implemented
+        </div>`
+        }
+        render = () => {
+            $("#todoForm").html(renderToForm());
+        }
+
+        addItem = () => { // add function
+            if ($(".input-text").val()) {
+                todoList.push({ id: generateUUID(), content: $(".input-text").val(), isChecked: false })
+                render();
+            } else {
+                alert("请填写添加内容");
+            }
+
+        }
+        render();
+        $(document).on("change", ".done-todo", function (event) { //on the change of check
+            if ($(this).is(":checked")) {
+                $(this).parent().addClass("checked");
+            } else {
+                $(this).parent().removeClass("checked");
+            }
+        });
+        $(document).on("click", "#filters a", function () { //hide different block
+
+            if ($(this).attr("data-filter") == "active") {
+
+                $('ol').children().filter('.checked').css('display', 'none');
+                $('ol').children().not('.checked').css('display', '');
+            }
+            if ($(this).attr("data-filter") == "all") {
+                $('ol').children().css('display', '');
+            }
+            if ($(this).attr("data-filter") == "complete") {
+                $('ol').children().filter('.checked').css('display', '');
+                $('ol').children().not(".checked").css('display', 'none');
+            }
+        });
+
+
+        $(document).on("click", "ol li", function () {
+            $(this).attr("contenteditable", true);
+        });
     });
